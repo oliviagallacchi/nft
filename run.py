7770693bd8c4da7a3ff9416053e827510985b17d
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-02-11 07:34:58 +0100

    Display of nft parts.

--- run.py
+++ run.py
@@ -4,13 +4,18 @@ import glob
 
 app = Flask(__name__)
 
-app.config["TEAMPLATES_AUTO_RELOAD"] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 
 @app.route("/")
 def home():
-    images = list(glob.glob("static/imgs/*.png"))
-    images = list(map(lambda x: x[7:], images))
+    images = {}
+    # types = list(glob.glob("static/imgs/*"))
+    types = ["arms", "bg"]
+    for t in types:
+        tmp = list(glob.glob(f"static/imgs/{t}/*.png"))
+        tmp = list(map(lambda x: x[7:], tmp))
+        images[t] = tmp
     return render_template("index.html", images=images)
 
 
