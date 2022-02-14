038c25b35d6f5cf8af1e97247cf868d376a3199b
Cedric Geissmann <cedric.geissmann@gmail.com>
2022-02-14 18:46:37 +0100

    Refactor a bit.

--- scrape.py
+++ scrape.py
@@ -14,26 +14,51 @@ def pick_url(img_list, url):
     return f"{url}{img['src']}"
 
 
-url = "http://localhost:5000"
+def get_bg_image(url):
+    res = requests.get(url)
+    soup = BeautifulSoup(res.content, "html.parser")
+    bgs = soup.select(".bg img")
+    r = requests.get(pick_url(bgs, url))
+    return Image.open(BytesIO(r.content))
 
-res = requests.get(url)
 
-soup = BeautifulSoup(res.content, "html.parser")
+def assemble_image():
+    # Get the background image. All the parts will be added onto this image.
+    bg = get_bg_image("http://localhost:5000")
 
-bgs = soup.select(".bg img")
-r = requests.get(pick_url(bgs, url))
-bg = Image.open(BytesIO(r.content))
+    # Store all the parts for assembling later
+    collection = []
 
-collection = []
+    # A list with all the servers we want to get parts from, and which parts we
+    # want.
+    li = [
+        {
+            "url": "http://localhost:5000",
+            "parts": [
+                ".arms",
+                ".shoes",
+            ]
+        }
+    ]
 
-for t in [".arms", ".shoes", ".hats"]:
-    tmp = soup.select(f"{t} img")
-    collection.append(pick_url(tmp, url))
+    # Iterate over the servers to collect all the parts. Append them to the
+    # collection
+    for server in li:
+        res = requests.get(server["url"])
+        soup = BeautifulSoup(res.content, "html.parser")
+        for t in server["parts"]:
+            tmp = soup.select(f"{t} img")
+            collection.append(pick_url(tmp, server["url"]))
 
-for url in collection:
-    r = requests.get(url)
-    tmp = Image.open(BytesIO(r.content))
-    bg.paste(tmp, (0,0), tmp)
+    # Iterate over the collection to assemble the image
+    for url in collection:
+        r = requests.get(url)
+        tmp = Image.open(BytesIO(r.content))
+        bg.paste(tmp, (0,0), tmp)
 
-filename = md5(str(datetime.timestamp(datetime.now())).encode()).hexdigest()
-bg.save(f"static/collection/{filename}.png", "PNG")
+    # Create a filename that does not yet exist and save the image.
+    filename = md5(str(datetime.timestamp(datetime.now())).encode()).hexdigest()
+    bg.save(f"static/collection/{filename}.png", "PNG")
+
+if __name__ == "__main__":
+    assemble_image()
